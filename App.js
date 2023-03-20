30578759f3a91a38a84016a22e81b34b8255543e
manishmp2it <manish.mp2it.mtr@gmail.com>
2023-03-20 17:55:02 +0530

    ok

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import products from './src/data/products';
 import ProductsScreen from "./src/screens/ProductsScreen"
+import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
+import ShopingCart from './src/screens/ShopingCart';
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <ProductsScreen/>
+     {/* <ProductsScreen/> */}
+     {/* <ProductDetailsScreen/> */}
+     <ShopingCart/>
 
       <StatusBar style='auto' />
     </View>
---
+++ src/screens/ShopingCart.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ShopingCart = () => {
+  return (
+    <View>
+      <Text>ShopingCart</Text>
+    </View>
+  )
+}
+
+export default ShopingCart
